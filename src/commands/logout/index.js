1d1d665da12331841109dae85cb47dc8fdc229bc
dusktrades <77290133+dusktrades@users.noreply.github.com>
2021-06-05 20:48:27 +0100

    Fixed logout command export

--- src/commands/logout/index.js
+++ src/commands/logout/index.js
@@ -1,12 +1,14 @@
 import { Logger } from '../../common/index.js';
 import { CONFIG } from '../../config/index.js';
 
-function logout() {
+function run() {
   CONFIG.USER.delete('API_KEY');
   CONFIG.USER.delete('API_SECRET');
   CONFIG.USER.delete('SUBACCOUNT');
 
   Logger.info('Removed stored API credentials');
 }
+
+const logout = { run };
 
 export { logout };
