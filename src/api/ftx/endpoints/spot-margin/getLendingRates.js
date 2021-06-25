1d74fe432d4fc60775a9a02d6be4c5eb4c2f1000
dusktrades <77290133+dusktrades@users.noreply.github.com>
2021-06-25 22:48:43 +0100

    Fixed get lending rates endpoint parameters

--- src/api/ftx/endpoints/spot-margin/getLendingRates.js
+++ src/api/ftx/endpoints/spot-margin/getLendingRates.js
@@ -2,12 +2,12 @@ import { composeHeaders } from '../composeHeaders.js';
 import { composeUrl } from '../composeUrl.js';
 import { request } from '../request.js';
 
-async function getLendingRates(exchange) {
+async function getLendingRates({ exchange }) {
   const endpoint = 'spot_margin/lending_rates';
   const url = composeUrl(exchange, endpoint);
   const headers = composeHeaders();
 
-  return request(url, headers);
+  return request({ url, headers });
 }
 
 export { getLendingRates };
