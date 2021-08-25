6a439921490051cb69368420a252012d51ad7f41
Dusk <77290133+dusktrades@users.noreply.github.com>
2021-08-25 17:41:53 +0100

    Consolidated command scheduling (new option: compound; removed option: repeat) (#25)

---
+++ bin/options/commands/compound/index.js
@@ -0,0 +1,7 @@
+const COMPOUND = {
+  FLAGS: '--compound',
+  DESCRIPTION:
+    'Enable auto-compounding. Proceeds from the lending offer(s) will be automatically compounded hourly by updating offered size(s) to the maximum lendable.',
+};
+
+export { COMPOUND };
--- bin/runCommand.js
+++ bin/runCommand.js
@@ -8,42 +8,55 @@ import { sleep } from '../src/util/index.js';
 import { handleError } from './handleError.js';
 import { notifyUpdate } from './notifyUpdate.js';
 
-// Repeat at XX:XX:00; used if no user/command-provided schedule.
-const FALLBACK_REPEAT_CRON_EXPRESSION = '* * * * *';
+function composeSchedule(schedule, compound) {
+  // Give `compound` option priority over `schedule` option.
+  if (compound) {
+    // Run at 59 minutes past every hour.
+    return { type: 'cron', cronExpression: '59 * * * *' };
+  }
 
-function getGlobalOptions() {
+  return schedule;
+}
+
+function getGlobalOptions(inlineCommandOptions) {
   const inlineGlobalOptions = program.opts();
 
   // Give inline options priority over stored config equivalents.
   return {
     exchange: inlineGlobalOptions.exchange ?? CONFIG.USER.get('EXCHANGE'),
+
     key: inlineGlobalOptions.key ?? CONFIG.USER.get('API_KEY'),
     secret: inlineGlobalOptions.secret ?? CONFIG.USER.get('API_SECRET'),
     subaccount: inlineGlobalOptions.subaccount ?? CONFIG.USER.get('SUBACCOUNT'),
 
-    schedule: inlineGlobalOptions.schedule,
-    repeat: inlineGlobalOptions.repeat,
-
-    enableReduceOnly:
-      inlineGlobalOptions.reduceOnly ?? CONFIG.USER.get('ENABLE_REDUCE_ONLY'),
-    enableIoc: inlineGlobalOptions.ioc ?? CONFIG.USER.get('ENABLE_IOC'),
-    enablePostOnly:
-      inlineGlobalOptions.postOnly ?? CONFIG.USER.get('ENABLE_POST_ONLY'),
-    enableRetry: inlineGlobalOptions.retry ?? CONFIG.USER.get('ENABLE_RETRY'),
-
-    rateLimit: inlineGlobalOptions.rateLimit ?? CONFIG.USER.get('RATE_LIMIT'),
+    schedule: composeSchedule(
+      inlineGlobalOptions.schedule,
+      inlineCommandOptions.compound
+    ),
 
     enableColours:
       inlineGlobalOptions.colour ?? CONFIG.USER.get('ENABLE_COLOURS'),
     enableUpdateNotifications:
       inlineGlobalOptions.updateNotifications ??
       CONFIG.USER.get('ENABLE_UPDATE_NOTIFICATIONS'),
+
+    /**
+     * Pseudo-global options: these will only affect the `trade` command, but
+     * it's easier to put them here as they're also configurable.
+     */
+    enableReduceOnly:
+      inlineGlobalOptions.reduceOnly ?? CONFIG.USER.get('ENABLE_REDUCE_ONLY'),
+    enableIoc: inlineGlobalOptions.ioc ?? CONFIG.USER.get('ENABLE_IOC'),
+    enablePostOnly:
+      inlineGlobalOptions.postOnly ?? CONFIG.USER.get('ENABLE_POST_ONLY'),
+    enableRetry: inlineGlobalOptions.retry ?? CONFIG.USER.get('ENABLE_RETRY'),
+    rateLimit: inlineGlobalOptions.rateLimit ?? CONFIG.USER.get('RATE_LIMIT'),
   };
 }
 
 function getOptions(inlineCommandOptions) {
   return {
-    global: getGlobalOptions(),
+    global: getGlobalOptions(inlineCommandOptions),
     command: inlineCommandOptions,
   };
 }
@@ -63,30 +76,17 @@ async function scheduleCommand(run, options) {
     return;
   }
 
-  logWaiting(options);
-
   // TODO: Add method of ending schedule (e.g. `--schedule-end`).
-  cron.schedule(options.global.schedule.cronExpression, () => {
-    run(options);
+  cron.schedule(options.global.schedule.cronExpression, async () => {
+    await run(options);
     logWaiting(options);
   });
+
+  logWaiting(options);
 }
 
-async function repeatRun(run, options, cronExpression) {
-  // Schedule repeat runs.
-  cron.schedule(cronExpression, async () => {
-    await run(options);
-  });
-
-  // Execute initial run.
-  await run(options);
-}
-
-async function runFunction(command, options) {
-  const {
-    run,
-    DEFAULT_REPEAT_CRON_EXPRESSION = FALLBACK_REPEAT_CRON_EXPRESSION,
-  } = Commands[command];
+async function runHandler(command, options) {
+  const { run } = Commands[command];
 
   if (options.global.schedule != null) {
     await scheduleCommand(run, options);
@@ -94,20 +94,7 @@ async function runFunction(command, options) {
     return;
   }
 
-  // TODO: Remove `repeat` option in favour of `schedule`.
-  if (options.global.repeat == null) {
-    await run(options);
-
-    return;
-  }
-
-  if (options.global.repeat === true) {
-    await repeatRun(run, options, DEFAULT_REPEAT_CRON_EXPRESSION);
-
-    return;
-  }
-
-  await repeatRun(run, options, options.global.repeat);
+  await run(options);
 }
 
 async function runCommand(command, inlineCommandOptions) {
@@ -118,7 +105,7 @@ async function runCommand(command, inlineCommandOptions) {
   }
 
   try {
-    await runFunction(command, options);
+    await runHandler(command, options);
   } catch (error) {
     handleError(error, options.global.enableColours);
   }
